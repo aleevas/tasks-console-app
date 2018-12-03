bd273b4be6a9d98ce82073d449d3fce1599128fd
Alexey Aleev <aleevas@gmail.com>
2018-12-03 17:04:47 +0200

    Added the DatabaseAdapter class

---
+++ src/DatabaseAdapter.php
@@ -0,0 +1,21 @@
+<?php
+namespace Acme;
+
+use PDO;
+
+class DatabaseAdapter {
+    protected $connection;
+
+    function __construct(PDO $connection)
+    {
+      $this->connection = $connection;   
+    }
+
+    public function fetchAll($tableName){
+        return $this->connection->query('select * from ' . $tableName)->fetchAll();
+    }
+
+    private function query($sql, $parametrs){
+        $this->connection->prepare($sql)->execute($parametrs);       
+    }
+}

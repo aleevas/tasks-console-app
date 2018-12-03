e62266fb0c4ad824c4b99eb689ac3a5165140e55
Alexey Aleev <aleevas@gmail.com>
2018-12-03 22:57:04 +0200

    Added using of db.sqlite as database

--- src/ShowCommand.php
+++ src/ShowCommand.php
@@ -31,13 +31,15 @@ class ShowCommand extends Command {
     }
 
     public function execute(InputInterface $input, OutputInterface $output) {
-
         $this->showTasks($output);
 
     }
     
     private function showTasks(OutputInterface $output){
-        $tasks = $this->database->fetchAll('tasks');
+       
+        if ( ! $tasks = $this->database->fetchAll('tasks')) {
+            return $output->writeln('<info>No tasks at the moment</info>');
+        }
         $table = new Table($output);
         $table->setHeaders(['ID', 'Description'])
               ->setRows($tasks)
